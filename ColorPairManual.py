9e747a118fa4c0a3e2913249d2f1a2e437e14b24
ccharan94 <77478060+ccharan94@users.noreply.github.com>
2021-02-23 23:32:44 +0530

    Update ColorPairManual.py

--- ColorPairManual.py
+++ ColorPairManual.py
@@ -1,1 +1,13 @@
+import ColorPairOperations
+
+#Print the Header
+print ("{:<20} {:<20} {:<20}".format('Pair Number','Major Color', 'Minor Color'))
+
+#Print the contents for Color code manual
+for majorcolor in ColorPairOperations.MAJOR_COLORS:
+  for minorcolor in ColorPairOperations.MINOR_COLORS:
+    pairnumber = get_pair_number_from_color(majorcolor,minorcolor)
+    print ("{:<20} {:<20} {:<20}".format(pairnumber, majorcolor, minorcolor))
+  
+  
 
